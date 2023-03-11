4decb59171c045c1712d7ce6c512c3d136a5bb34
salih <s.abdrazakova95@gmail.com>
2023-03-11 21:31:04 +0300

    trying to push the code

--- src/main/java/first_git_package/MySecondClass.java
+++ src/main/java/first_git_package/MySecondClass.java
@@ -4,6 +4,7 @@ public class MySecondClass {
 
     public static void main(String[] args) {
         System.out.println("This is my second class");
+        System.out.println("This is my     second class");
         System.out.println("UPDATING EXISTING CODE");
     }
 }
